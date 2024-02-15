c83d0bb3f91f8aa3d40a73b5c250828a8cbb1f72
Károly Lónich <karoly.lonich@smartcoder.dev>
2024-02-15 11:00:15 +0100

    feat: refactor naming

--- install.ps1
+++ install.ps1
@@ -1,11 +1,11 @@
-# Define the path to the VirtualDesktopIndicator.exe file
-$exePath = Join-Path -Path $PSScriptRoot -ChildPath "VirtualDesktopIndicator.exe"
+# Define the path to the virtual-desktop-indicator.exe file
+$exePath = Join-Path -Path $PSScriptRoot -ChildPath "virtual-desktop-indicator.exe"
 
 # Define the path to the startup folder
 $startupFolderPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 
-# Create a shortcut for VirtualDesktopIndicator.exe
-$shortcutPath = Join-Path -Path $PSScriptRoot -ChildPath "VirtualDesktopIndicator.lnk"
+# Create a shortcut for virtual-desktop-indicator.exe
+$shortcutPath = Join-Path -Path $PSScriptRoot -ChildPath "virtual-desktop-indicator.lnk"
 $WshShell = New-Object -ComObject WScript.Shell
 $shortcut = $WshShell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = $exePath
